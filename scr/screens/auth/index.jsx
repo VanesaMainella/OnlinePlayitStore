abf3e88facbec799292dec4dbd0a35c0b8878c7e
VanesaMainella <vanemainella.19@gmail.com>
2023-08-25 21:19:48 -0300

    add

--- scr/screens/auth/index.jsx
+++ scr/screens/auth/index.jsx
@@ -1,13 +1,46 @@
+import { useState } from "react";
 import { styles } from "./styles";
-import {View, Text} from "react-native";
-
+import {View, Text, TouchableOpacity} from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import { COLORS } from '../../themes';
 
 const Auth = () => {
-    return (
+    const [isLogin, setIsLogin] = useState (true)
+    const headerTitle = isLogin ? "Login" : "Register";
+    const buttonTitle = isLogin ? "Login" : "Register";
+    const messageText = isLogin ? "Need an account?" : "Already have an account?";
+        return (
         <View style = {styles.container}>
-            <Text>
-                Auth
-            </Text>
+           <View style={styles.content}>
+           <Text style={styles.header}>{headerTitle}</Text>
+           <Text style={styles.label}>Email</Text>
+           <TextInput style={styles.input} 
+                      placeholder="email@domain.com" 
+                      placeholderTextColor={COLORS.grey} 
+                      autoCapitalize="none"
+                      autoCorrect={false}
+                      onChangeText={()=>{}}
+                      />
+           <Text style={styles.label}>Password</Text>
+           <TextInput style={styles.input} 
+                      placeholder="**********" 
+                      placeholderTextColor={COLORS.grey} 
+                      autoCapitalize="none"
+                      autoCorrect={false}
+                      onChangeText={()=>{}}
+                      secureTextEntry={true}
+                      />
+                      <View style={styles.linkContainer}>
+                        <TouchableOpacity style={styles.link} onPress={()=> setIsLogin (!isLogin)}>
+                            <Text style={styles.linkText}>messageText</Text>
+                        </TouchableOpacity>
+                      </View>
+                      <View style={styles.buttonContainer}>
+                        <TouchableOpacity style={styles.button}>
+                            <Text style={styles.buttonText}>{buttonTitle}</Text>
+                        </TouchableOpacity>
+                      </View>
+           </View>
         </View>
     );
 };
